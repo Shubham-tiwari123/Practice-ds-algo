991c4ddc005d2a726fc64b6b035548fe3db6f161
Shubham-tiwari123 <st540362@gmail.com>
2019-09-19 21:46:38 +0530

    tree ques

--- BinaryTree/BinaryTree.java
+++ BinaryTree/BinaryTree.java
@@ -1,7 +1,9 @@
 package practicetree;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class BinaryTree {
     //DLR
@@ -94,7 +96,7 @@ public class BinaryTree {
                 }
                 else{
                     TreeNode newNode = new TreeNode(val);
-                    temp.left=newNode;
+                    temp.right=newNode;
                     break;
                 }
             }
@@ -164,5 +166,182 @@ public class BinaryTree {
                 q.add(temp.right);
             }
         }
+    }
+    
+    void printLevelOrderReverse (TreeNode node){
+        Stack<Integer> ans = new Stack<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(node);
+        while(!q.isEmpty()){
+            TreeNode temp = q.poll();
+            ans.push(temp.data);
+            if(temp.right!=null)
+                q.add(temp.right);
+            if(temp.left!=null)
+                q.add(temp.left);
+        }
+        System.out.print("\nReverse lvel order:-");
+        while(!ans.empty())
+            System.out.print(" "+ans.pop());
+    }
+    
+    void deepestNode(TreeNode node){
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(node);
+        int lastNode = 0;
+        while(!q.isEmpty()){
+            TreeNode temp = q.poll();
+            lastNode = temp.data;
+            if(temp.left!=null)
+                q.add(temp.left);
+            if(temp.right!=null)
+                q.add(temp.right);
+        }
+        System.out.print("\nLast node:-"+lastNode);
+    }
+    
+    void totalLeaves(TreeNode node){
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(node);
+        int count = 0;
+        while(!q.isEmpty()){
+            TreeNode temp = q.poll();
+            if(temp.left==null && temp.right==null)
+                count++;
+            if(temp.left!=null)
+                q.add(temp.left);
+            if(temp.right!=null)
+                q.add(temp.right);
+        }
+        System.out.print("\nTotal leave nodes:-"+count);
+    }
+    
+    void fullNodes(TreeNode node){
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(node);
+        int count = 0;
+        while(!q.isEmpty()){
+            TreeNode temp = q.poll();
+            if(temp.left!=null && temp.right!=null)
+                count++;
+            if(temp.left!=null)
+                q.add(temp.left);
+            if(temp.right!=null)
+                q.add(temp.right);
+        }
+        System.out.print("\nTotal full nodes:-"+count);
+    }
+    
+    void halfNodes(TreeNode node){
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(node);
+        int count = 0;
+        while(!q.isEmpty()){
+            TreeNode temp = q.poll();
+            if(temp.left!=null && temp.right==null)
+                count++;
+            if(temp.left==null && temp.right!=null)
+                count++;
+            if(temp.left!=null)
+                q.add(temp.left);
+            if(temp.right!=null)
+                q.add(temp.right);
+        }
+        System.out.print("\nHalf nodes:-"+count);
+    }
+    
+    void checkTwoTreeStructuralIdentical(TreeNode head1, TreeNode head2){
+        Queue<TreeNode> q1 = new LinkedList<>();
+        Queue<TreeNode> q2= new LinkedList<>();
+        q1.add(head1);
+        q2.add(head2);
+        boolean flag=true;
+        while(!q1.isEmpty()){
+            TreeNode temp1 = q1.poll();
+            TreeNode temp2 = q2.poll();
+            
+            if((temp1.left==null && temp2.left!=null)||(temp1.right==null && temp2.right!=null)){
+                flag =false;
+                break;
+            }
+            if((temp1.left!=null && temp2.left==null)||(temp1.right!=null && temp2.right==null)){
+                flag =false;
+                break;
+            }
+            if(temp1.left!=null && temp2.left!=null){
+                q2.add(temp2.left);
+                q1.add(temp1.left);
+            }
+            if(temp1.right!=null && temp2.right!=null){
+                q2.add(temp2.right);
+                q1.add(temp1.right);
+            }
+        }
+        if(flag)
+            System.out.print("\nTrees are structural identical");
+        else
+            System.out.print("\nTrees are not structural identical");
+    }
+    
+    void levelThatHasMaxSum(TreeNode node){
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(node);
+        q.add(null);
+        int currSum = 0;
+        int maxSum=0;
+        while(!q.isEmpty()){
+            TreeNode temp = q.poll();
+            if(temp!=null){
+                currSum=currSum+temp.data;
+                if(temp.left!=null)
+                    q.add(temp.left);
+                if(temp.right!=null)
+                    q.add(temp.right);
+            }
+            else{
+                if(currSum>=maxSum){
+                    maxSum=currSum;
+                }
+                currSum=0;
+                if(!q.isEmpty())
+                    q.add(null);
+            }
+        }
+        System.out.print("\nMax sum: "+maxSum);
+        
+    }
+    
+    void printRootToLeaf(TreeNode root){
+        Stack<TreeNode> s = new Stack<>();
+        HashMap<TreeNode,TreeNode> parent = new HashMap<>();
+        s.push(root);
+        parent.put(root, null);
+        while(!s.isEmpty()){
+            TreeNode temp = s.pop();
+            if(temp.left==null && temp.right==null)
+                printNode(temp, parent);
+            
+            if(temp.right!=null){
+                s.push(temp.right);
+                parent.put(temp.right, temp);
+            }
+            if(temp.left!=null){
+                s.push(temp.left);
+                parent.put(temp.left, temp);
+            }
+        }
+    }
+    void printNode(TreeNode cur,HashMap<TreeNode,TreeNode> parent){
+        Stack<TreeNode> ans = new Stack<>();
+        while(cur!=null){
+            ans.push(cur);
+            cur=parent.get(cur);
+        }
+        System.out.print("\n");
+        while(!ans.isEmpty()){
+            cur=ans.pop();
+            System.out.print(cur.data+" ");
+        }
+        
     }
 }
--- BinaryTree/PracticeTree.java
+++ BinaryTree/PracticeTree.java
@@ -15,18 +15,30 @@ class TreeNode{
 public class PracticeTree {
 
     TreeNode createBinaryTree(){
-        TreeNode root = new TreeNode(0);
-        root.left = new TreeNode(1);
-        root.right = new TreeNode(2);
-        root.left.right=new TreeNode(4);
-        root.left.left=new TreeNode(3);
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left=new TreeNode(4);
+        root.left.right=new TreeNode(5);
+        root.right.left=new TreeNode(6);
         return root;
+    }
+    TreeNode createSecBinaryTree(){
+        TreeNode root2 = new TreeNode(10);
+        root2.left = new TreeNode(2);
+        root2.right = new TreeNode(3);
+        root2.left.left=new TreeNode(4);
+        root2.left.right=new TreeNode(5);
+        root2.right.left=new TreeNode(6);
+        root2.right.right=new TreeNode(7);
+        return root2;
     }
     
     public static void main(String[] args) {
         PracticeTree pt = new PracticeTree();
         BinaryTree bt = new BinaryTree();
         TreeNode head = pt.createBinaryTree();
+        
         System.out.print("\nPreOrder:-");
         bt.preOrder(head);
         System.out.print("\nInOrder:-");
@@ -37,11 +49,25 @@ public class PracticeTree {
         bt.levelOrder(head);
         bt.findMax(head);
         bt.searchElement(head, 4);
-        bt.insertElement(head, 15);
+        bt.insertElement(head, 7);
         bt.sizeOfTree(head);
         bt.deleteElement(head, 1);
         System.out.print("\nTree after deletion:-");
         bt.levelOrder(head);
+        bt.printLevelOrderReverse(head);
+        bt.deepestNode(head);
+        bt.totalLeaves(head);
+        bt.fullNodes(head);
+        bt.halfNodes(head);
+        
+        TreeNode root1 = pt.createBinaryTree();
+        TreeNode root2 = pt.createSecBinaryTree();
+        
+        bt.checkTwoTreeStructuralIdentical(root1, root2);
+        bt.levelThatHasMaxSum(root2);
+        bt.printRootToLeaf(root1);
+        
+        
     }
     
 }
