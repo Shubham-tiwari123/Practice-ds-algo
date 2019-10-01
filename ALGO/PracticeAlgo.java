3d9c215b9ce0fefa37479247d20780eb03e42adb
Shubham-tiwari123 <st540362@gmail.com>
2019-10-01 22:48:22 +0530

    Main file

--- ALGO/PracticeAlgo.java
+++ ALGO/PracticeAlgo.java
@@ -1,5 +1,9 @@
 package practicealgo;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class PracticeAlgo {
 
     static void minuteToWinIt(int[] a, int k) {
@@ -34,11 +38,87 @@ public class PracticeAlgo {
             System.out.print(a[i]+" ");
     }
     
+    static void merger(List<Integer> p,int l,int m,int r){
+        int n1 = m-l+1;
+        int n2 = r-m;
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+        
+        for (int i=0; i<n1; ++i) 
+            L[i] = p.get(l + i); 
+        for (int j=0; j<n2; ++j) 
+            R[j] = p.get(m + 1+ j); 
+        
+        int i = 0, j = 0;
+        int k = l; 
+        while (i < n1 && j < n2) 
+        { 
+            if (L[i] <= R[j]) 
+            { 
+                p.remove(k);
+                p.add(k, L[i]);
+                i++; 
+            } 
+            else
+            { 
+                p.remove(k);
+                p.add(k, R[j]); 
+                j++; 
+            } 
+            k++; 
+        }
+        while (i < n1) 
+        { 
+            p.remove(k);
+            p.add(k, L[i]);
+            i++; 
+            k++; 
+        }
+        while (j < n2) 
+        { 
+            p.remove(k);
+            p.add(k, R[j]); 
+            j++; 
+            k++; 
+        } 
+    }
+    
+    static void sort(List<Integer> p,int l,int r){
+        if(l<r){
+            int m= (l+r)/2;
+            sort(p, l, m);
+            sort(p, m+1, r);
+            merger(p, l, m, r);
+        }
+    }
+    static void solve(List<Integer> p){
+        p.add(0);
+        sort(p, 0, p.size()-1);
+        int sum=0;
+        for(int i=0;i<p.size()-1;i++){
+            int diff = Math.abs(p.get(i)-p.get(i+1));
+            sum = sum+diff;
+        }
+        System.out.println("sum:-"+sum);
+    }
     public static void main(String[] args) {
         DPquestion dp = new DPquestion();
-        //dp.longestCommenSequence("shubham", "shivam");
-        //dp.longestCommenString("GeeksforGeeks", "GeeksQuiz");
-        dp.longestPalindromicSubstring("geeksskeeg");
+        /*dp.longestCommenSubSequence("shubham", "shivam");
+        dp.longestCommenSubString("GeeksforGeeks", "GeeksQuiz");
+        dp.longestPalindromicSubstring("GEEKSFORGEEKS");
+        dp.longestPalindromicSubsequence("GEEKSFORGEEKS");
+        dp.longestRepeatedSubSeq("AABEBCDD");
+        int mat[]={2,3,5,10};
+        dp.coinChange(mat, 15);
+        int mat1[]={5,7,8,9};
+        dp.minCoinRequired(mat1, 49);*/
+        PracticeAlgo pc = new PracticeAlgo();
+        List<Integer> p = new ArrayList<>();
+        p.add(2);
+        p.add(1);
+        p.add(4);
+        p.add(3);
+        pc.solve(p);
         
     }
     
